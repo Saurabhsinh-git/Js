530a54d6b84e7325fb836c81302bd41d899fc94a
Saurabhsinh-git <127668812+Saurabhsinh-git@users.noreply.github.com>
2024-07-14 12:08:43 +0530

    day 1 completed with 5 videos

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accId = 9876
+let acEmail = "srz@1.com"
+var accPass = "3257"
+acCity = "Ahmedabad"
+let acState
+// accId = 2
+acEmail = "oyoy"
+accPass = "8686"
+acCity = "RJ"
+
+console.log(accId)
+console.table([acEmail, accPass, acCity, acState])
---
+++ 02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as a newer version
+
+// alert(3 + 3) // we are using node  js not browser
+const srz = true
+/* 
+data types in Js (primitive)
+number => 2 to the power 53
+bigint
+boolean=> true/false
+string
+undefined
+null=> standalone value
+symbol => unique
+
+data types in Js (non-primitive)
+objects
+*/
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
