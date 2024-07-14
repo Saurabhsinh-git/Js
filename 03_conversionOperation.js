c4cb52e8eae6cc3a4abdb23f8deb4182dd3c875e
Saurabhsinh-git <127668812+Saurabhsinh-git@users.noreply.github.com>
2024-07-14 12:39:46 +0530

    7

---
+++ 03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = true
+
+console.log(typeof score)
+
+let valueNum = Number(score)
+// console.log(typeof valueNum)
+// console.log(score)
+// console.log(valueNum)
+let isLogIn = "a"
+let isLogInN = Boolean(isLogIn)
+console.log(typeof isLogInN)
+console.log(isLogInN)
+
+/* 
+1 true
+0 false
+"" false
+"a" true
+*/
